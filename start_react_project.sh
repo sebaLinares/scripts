cc6d1078d7e9519431ad54168dd85e7ef37058ad
Sebastian Linares <slinaresl@gmail.com>
2019-07-30 18:13:21 -0400

    fix: 2 fixes
    first: enter de directory recently created by create react-app. second:
    add babel-eslint && modify .eslintrc to use it

--- start_react_project.sh
+++ start_react_project.sh
@@ -7,8 +7,15 @@
 echo Name of your project?
 read name
 
+echo // ====================== //
+echo // CREATING REACT PROJECT //
+echo // ====================== //
 # Create react project with $name
-yarn create react-app $name
+yarn create react-app $name && cd ./$name
+
+echo // ======================== //
+echo //  Installing dependencies //
+echo // ======================== //
 
 # Install packages
 yarn add eslint prettier -D
@@ -18,6 +25,9 @@ yarn add husky lint-staged -D
 yarn add @commitlint/cli -D
 yarn add @commitlint/config-conventional -D
 yarn add babel-eslint -D
+echo // =================== //
+echo //  .prettierrc config //
+echo // =================== //
 
 # Create .prettierrc
 
@@ -25,6 +35,10 @@ echo "{
   'printWidth': 100,
   'singleQuote': true
 }" > .prettierrc
+
+echo // =================== //
+echo // package.json config //
+echo // =================== //
 
 # Add lint scripts to package.json
 jq '.scripts +=
@@ -44,17 +58,24 @@ jq '.husky += {
   }' package.json > temp.txt && mv temp.txt package.json
 
 
-jq '.lint-staged += {
+jq '."lint-staged" += {
       "*.js": [
        "yarn run lint:fix",
        "git add"
       ]
   }' package.json > temp.txt && mv temp.txt package.json
 
+echo // ================= //
+echo // commitlint config //
+echo // ================= //
+
 # Create commitlint.config.js
 echo "module.exports = {extends: ['@commitlint/config-conventional']};" > commitlint.config.js
 
 
+echo // ================ //
+echo // .eslintrc config //
+echo // ================ //
 # create .eslintrc
 eslint --init
 
@@ -73,3 +94,10 @@ jq '.rules += {
 
 # Add babel-eslint to "parser" in .eslintrc
 jq '.parser += "babel-eslint"' .eslintrc.json > temp.txt && mv temp.txt .eslintrc.json
+
+echo // ====================== //
+echo // .env config //
+echo // ====================== //
+
+
+echo "SKIP_PREFLIGHT_CHECK=true" > .env
